8be7d2a7666243fd67542f2af33e29bfbc984d20
RM40434 <malhotra.rajiv@outlook.com>
2020-09-04 11:33:37 -0500

    Updated the comments in camper_age_input.py.

--- main/camper_age_input.py
+++ main/camper_age_input.py
@@ -3,8 +3,8 @@ Program: camper_age_input.py
 Author: Rajiv Malhotra
 Last Modified Date: 09/03/2020
  
-The program will prompt for the age of one infant (age 1-5 years) who is attending camp 
-and convert the age in months to years via a function call then print the result.
+The program will prompt for the age of camper who is attending camp 
+and convert the age in years to months via a function call then print the result.
 """""
 import constants
 
