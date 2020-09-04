e2d1900c8ea7ec55105cc66c702299b11629f1cf
RM40434 <malhotra.rajiv@outlook.com>
2020-09-04 11:06:29 -0500

    Pushing first version of camper_age_input.py.

---
+++ main/camper_age_input.py
@@ -0,0 +1,14 @@
+"""""
+Program: camper_age_input.py
+Author: Rajiv Malhotra
+Last Modified Date: 09/03/2020
+ 
+The program will prompt for the age of one infant (age 1-5 years) who is attending camp 
+and convert the age in months to years via a function call then print the result.
+"""""
+import constants
+
+def convert_to_months(x):
+    pass
+
+if __name__ == '__main__':
---
+++ test/test_functions.py
@@ -0,0 +1,11 @@
+import unittest
+from main import camper_age_input
+
+
+class FunctionTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(False, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
